d114b2523b2a2ca5a13bc199a44b2bc85adaec97
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-31 13:37:12 -0500

    Create collecting_data_survey_server.R

---
+++ collecting_data_survey_server.R
@@ -0,0 +1,15 @@
+# server.R
+
+library(shiny)
+shinyServer(
+	function(input, output){
+		output$oid1 <- renderPrint({input$id1})  		#get html label
+		output$oid2 <- renderPrint({input$id2}) 		
+		output$odate <- renderPrint({input$date})		
+	}
+)
+
+ 
+ 
+ 
+ 
