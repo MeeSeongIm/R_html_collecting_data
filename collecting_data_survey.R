6beef1a9a68510bf4f50e364945528bb9f42c293
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-31 13:40:20 -0500

    Update collecting_data_survey.R

--- collecting_data_survey.R
+++ collecting_data_survey.R
@@ -4,17 +4,17 @@ library(shiny)
 shinyUI(pageWithSidebar(
 	headerPanel("Collecting data"),
 	sidebarPanel(
-    h3("Technical Skills"),
+    		h3("Technical Skills"),
 		numericInput("id1", "Rate yourself on a scale of 1 to 10 of your technical skills, where 1 is least technical and 10 is most technical.", 1, min = 1, max = 10, step =1),
 		h3("Movies"),
-    checkboxGroupInput("id2", "Choose your top 3 favorite movies.",
+    		checkboxGroupInput("id2", "Choose your top 3 favorite movies.",
 			c("12 Years a Slave" = "1",
 			"Captain Phillips" = "2",
 			"Gravity" = "3",
-      "American Hustle" = "4",
-      "Her" = "5",
-      "The Wolf of Wall Street" = "6",
-      "Les Miserables" = "7"
+      			"American Hustle" = "4",
+      			"Her" = "5",
+      			"The Wolf of Wall Street" = "6",
+      			"Les Miserables" = "7"
 			),
 		dateInput("date", "Date:")
 		) 
